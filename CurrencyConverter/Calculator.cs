b21f192e1edd57507edd3b67defeea23628ceab8
Urszula Rudnicka <urudnicka400@gmail.com>
2022-02-19 06:26:46 +0100

    Finished main functionalities

---
+++ CurrencyConverter/Calculator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace CurrencyConverter
+{
+    class Calculator
+    {
+        public static string Date { get; set; }
+        public static Dictionary<string, double> Rates { get; set; }
+
+        public Calculator()
+        {
+            ReadDataFile();
+        }
+
+        public static double ConvertEUR(double value, string currency)
+        {
+            double rate = Rates[currency];
+            return value * rate;
+        }
+
+        public static bool IsItACurrency(string currency)
+        {
+            return Rates.ContainsKey(currency);
+        } 
+
+        public static void ListCurrencies()
+        {
+            Rates.Keys.ToList().ForEach(c => Console.Write("{0} ", c));
+        }
+
+        private static void ReadDataFile()
+        {
+            Rates = new Dictionary<string, double>();
+
+            string filePath = @"..\..\Data\exchange_rates.xml";
+
+            if (!File.Exists(filePath))
+            {
+                CreateDataFile(filePath);
+            }
+
+            XmlReader xmlReader = XmlReader.Create(filePath);
+
+            while (xmlReader.Read())
+            {
+                if ((xmlReader.NodeType == XmlNodeType.Element) && (xmlReader.Name == "Cube"))
+                {
+                    if (xmlReader.HasAttributes)
+                    {
+                        if (xmlReader.GetAttribute("time") != null)
+                            Date = xmlReader.GetAttribute("time");
+                        else
+                            Rates[xmlReader.GetAttribute("currency")] = Convert.ToDouble(xmlReader.GetAttribute("rate"));
+                    }
+                }
+            }
+        }
+
+        private static void CreateDataFile(string path)
+        {
+            Directory.CreateDirectory(@"..\..\Data");
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load("https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml");
+
+            File.WriteAllText(path, xmlDoc.InnerXml);
+        }
+
+    }
+}
--- CurrencyConverter/Program.cs
+++ CurrencyConverter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CurrencyConverter
 {
@@ -6,7 +7,27 @@ namespace CurrencyConverter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            new Calculator();
+
+            Console.WriteLine("Docelowa waluta: ");
+            string currency = Console.ReadLine().ToUpper();
+
+            if (!Calculator.IsItACurrency(currency))
+            {
+                Console.WriteLine("This is not a valid currency.");
+                Console.WriteLine("The list of currencies includes:");
+                Calculator.ListCurrencies();
+                return;
+            }
+
+            Console.WriteLine("Wartosc w EUR: ");
+            double value = Convert.ToDouble(Console.ReadLine());
+
+            double result = Calculator.ConvertEUR(value, currency);
+
+            Console.WriteLine($"{value} EUR = {result} {currency}");
+            Console.WriteLine("Kurs z dnia " + Calculator.Date); 
+
         }
     }
 }
